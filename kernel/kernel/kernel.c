32c7699a7a20c56512617ab0c9fe0dd148fd0fa1
SenseiSayaka <marat3112965@mail.ru>
2025-02-22 12:27:32 +0300

    added gdt

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
-
+#include <stdint.h>
 #include <kernel/tty.h>
 
+void test_gdt(void){
+	uint16_t cs, ds;
+
+    // Получаем текущие значения сегментных регистров
+    __asm__ volatile("mov %%cs, %0" : "=r"(cs));
+    __asm__ volatile("mov %%ds, %0" : "=r"(ds));
+
+    // Если CS == 0x08 и DS == 0x10, значит GDT загружена корректно
+    if (cs == 0x08 && ds == 0x10) {
+        printf("GDT loaded successfully!\n");
+		printf(" ")
+        printf("Segment registers updated.\n");
+		printf(" ")
+    } else {
+        printf("GDT failed to load!\n");
+		printf(" ")
+    }
+}
 void kernel_main(void) {
 	terminal_initialize();
+	test_gdt();
 	printf("Welcome to kernel of Codeshell\n");
+	printf(" ")
 
-	float numb = 71.8540372658965367;
-	printf("number = %f ", numb);
 }
