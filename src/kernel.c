293531f2fe8729f2234442b6e969d5fba0d1aca6
SenseiSayaka <marat3112965@mail.ru>
2025-10-18 17:03:26 +0300

    keyboard driver

--- src/kernel.c
+++ src/kernel.c
@@ -3,6 +3,8 @@
 #include "idt.h"
 #include "timer.h"
 #include "util.h"
+#include "stdlib/stdio.h"
+#include "keyboard.h"
 void kmain(void);
 void set_screen_color(uint8_t color);
 
@@ -15,12 +17,19 @@ void set_screen_color(uint8_t color) {
 }
 
 void kmain(void) {
+  set_screen_color(0x1F);
   initGdt();
   print("GDT is done\r\n");
   initIdt();
   print("IDT is done\r\n");
   initTimer();
-  print("Timer is done\r\n"); 
+  print("Timer is done\r\n");
+  initKeyboard();
+  print("Keyboard is done\r\n");
+  print("##########################\n");
+  print("#        Codeshell       #\n");
+  print("##########################\n");
+  print("cshell-->"); 
   set_screen_color(0x1F);
   for(;;);
 }
